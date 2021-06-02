6a380c04afaeb9098c07837d1b7bdbe60f659b40
K-nunuhara <k.nunuhara@gmail.com>
2021-06-02 15:21:41 +0900

    update document: Class 6 is finished for LifingEntities.

--- MyGame01_ecosystem/Assets/Scripts/LivingEntities/Sheep.cs
+++ MyGame01_ecosystem/Assets/Scripts/LivingEntities/Sheep.cs
@@ -8,14 +8,72 @@ public class Sheep : Herbivore
     public Rigidbody rb = null;
     public GameObject target = null;
 
+    public float minScale = 0.1f;
+    public float maxScale = 1.0f;
+    public float growSpan = 30f;
+    public float soundSpan = 7f;
+    public float currentTime = 0f;
+    public float growRate = 0.1f;
+
     void Start()
     {
         Initialize();
     }
 
+    public override void Grow()
+    {
+        float nextScale = Mathf.Min(this.transform.localScale.x + growRate, maxScale);
+        this.transform.localScale = Vector3.one * nextScale;
+        this.age++;
+    }
+
     void Update()
     {
         ConsumeCalorie();
+
+        if (this.health <= this.stats.MIN_HEALTH)
+        {
+            Die();
+        }
+
+        if (this.calorie < this.stats.BASE_CALORIE)
+        {
+            // Hunder class doesn't exsist now
+            // ChangeState(new Hunger());
+        }
+        else
+        {
+            ChangeState(new Normal());
+        }
+
+        currentTime += Time.deltaTime;
+        if (currentTime > growSpan)
+        {
+            currentTime = 0f;
+            Grow();
+        }
+        if ((int)currentTime % (int)soundSpan == 0)
+        {
+            MakeSound();
+        }
+    }
+
+    public override void ConsumeCalorie()
+    {
+        Debug.Log("UID: " + this.uid + " calorie is " + this.calorie);
+        Debug.Log("UID: " + this.uid + " health is " + this.health);
+        this.calorie = Mathf.Max(this.calorie - this.stats.CONSUME_CALORIE * Time.deltaTime, this.stats.MIN_CALORIE);
+        if (this.calorie == this.stats.MIN_CALORIE)
+        {
+            // Damage
+            this.health = Mathf.Max(this.health - Time.deltaTime, this.stats.MIN_HEALTH);
+        }
+        else
+        {
+            // Heal
+            this.health = Mathf.Min(this.health + Time.deltaTime, this.stats.MAX_HEALTH);
+
+        }
     }
 
     private void FixedUpdate()
@@ -40,16 +98,6 @@ public class Sheep : Herbivore
     }
 
     public override void Ruminant()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public override void Grow()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public override void ConsumeCalorie()
     {
         throw new System.NotImplementedException();
     }
